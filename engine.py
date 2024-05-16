e44e6f48baf8819751a5819966fa43ddb9f0bfc0
Guillermo <guillermosdlg@gmail.com>
2024-05-16 10:03:20 -0600

    Esqueleto del modelo de objetos

---
+++ engine.py
@@ -0,0 +1,44 @@
+class engine():
+    def __init__(self):
+        self.matrix = [[0,0,0,0,0,0,0,0,0,0,0,0],
+                       [0,0,0,0,0,0,0,0,0,0,0,0],
+                       [0,0,0,0,0,0,0,0,0,0,0,0],
+                       [0,0,0,0,0,0,0,0,0,0,0,0],
+                       [0,0,0,0,0,0,0,0,0,0,0,0],
+                       [0,0,0,0,0,0,0,0,0,0,0,0],
+                       [0,0,0,0,0,0,0,0,0,0,0,0],
+                       [0,0,0,0,0,0,0,0,0,0,0,0],
+                       [0,0,0,0,0,0,0,0,0,0,0,0],
+                       [0,0,0,0,0,0,0,0,0,0,0,0],
+                       [0,0,0,0,0,0,0,0,0,0,0,0],
+                       [0,0,0,0,0,0,0,0,0,0,0,0]]
+
+    def uploadImg(self):
+        pass
+
+    def editImg(self):
+        pass
+
+    def viewImg(self):
+        pass
+
+    def viewMatNum(self):
+        pass
+
+    def closeImg(self):
+        pass
+
+    def showImg(self):
+        pass
+
+    def zoomIn(self):
+        pass
+
+    def zoomOut(self):
+        pass
+
+    def rotateMatRgt(self):
+        pass
+
+    def rotateMatLft(self):
+        pass
