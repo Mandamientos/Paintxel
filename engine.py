5174e156d616517840b246c176595d4a2add503c
Guillermo <guillermosdlg@gmail.com>
2024-05-17 11:00:43 -0600

    Avance de la interfaz gráfica

--- engine.py
+++ engine.py
@@ -1,26 +1,44 @@
-class engine():
+import pygame
+import tkinter as tk
+from tkinter import filedialog
+
+
+class Engine:
     def __init__(self):
-        self.matrix = [[0,0,0,0,0,0,0,0,0,0,0,0],
-                       [0,0,0,0,0,0,0,0,0,0,0,0],
-                       [0,0,0,0,0,0,0,0,0,0,0,0],
-                       [0,0,0,0,0,0,0,0,0,0,0,0],
-                       [0,0,0,0,0,0,0,0,0,0,0,0],
-                       [0,0,0,0,0,0,0,0,0,0,0,0],
-                       [0,0,0,0,0,0,0,0,0,0,0,0],
-                       [0,0,0,0,0,0,0,0,0,0,0,0],
-                       [0,0,0,0,0,0,0,0,0,0,0,0],
-                       [0,0,0,0,0,0,0,0,0,0,0,0],
-                       [0,0,0,0,0,0,0,0,0,0,0,0],
-                       [0,0,0,0,0,0,0,0,0,0,0,0]]
+        self.rows = 30
+        self.cols = 30
+        self.cSize = 20
+        self.matrix = [[0 for _ in range(self.cols)] for _ in range(self.rows)]
+
+    def createMat(self, root):
+        for i in range(len(self.matrix)):
+            for j in range(len(self.matrix[i])):
+                rect = pygame.Rect(300 + j * self.cSize, 175 + i * self.cSize, self.cSize, self.cSize)
+                pygame.draw.rect(root, (255, 255, 255), rect)
+                pygame.draw.rect(root, (55, 89, 89), rect, 2)
+
+
+    def drawOverMat(self):
+        if pygame.mouse.get_pressed()[0]:
+            pass
+
 
     def uploadImg(self):
+        window = tk.Tk()
+        window.withdraw()
+        path = filedialog.askopenfilename()
+        if path == "":
+            return
+        print(path)
+
+    def saveImg(self):
         pass
 
     def editImg(self):
         pass
 
     def viewImg(self):
-        pass
+        print(self.matrix)
 
     def viewMatNum(self):
         pass
@@ -42,3 +60,32 @@ class engine():
 
     def rotateMatLft(self):
         pass
+
+
+class Button:
+    def __init__(self, x, y, image, root):
+        self.image = pygame.transform.scale(image, (50, 50))
+        self.root = root
+        self.x = x
+        self.y = y
+        self.clicked = False
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x, y)
+
+    def draw(self):
+        self.root.blit(self.image, (self.x, self.y))
+
+    def pressed(self):
+
+        command = False
+        mousePos = pygame.mouse.get_pos()
+
+        if self.rect.collidepoint(mousePos):
+            if pygame.mouse.get_pressed()[0] and not self.clicked:
+                command = True
+                self.clicked = True
+
+        if not pygame.mouse.get_pressed()[0]:
+            self.clicked = False
+
+        return command
--- main.py
+++ main.py
@@ -1,2 +1,55 @@
 import pygame
-from engine import *
+from engine import Engine, Button
+
+pygame.init()
+
+Eng = Engine()
+icon = pygame.image.load("assets/images/brush.png")
+root = pygame.display.set_mode((1200, 800))
+pygame.display.set_caption("Paintxel")
+pygame.display.set_icon(icon)
+root.fill((223, 223, 229))
+
+# --> Elementos de la interfaz gráfica
+
+nav_rect = pygame.Rect(0, 0, 1200, 150)
+pygame.draw.rect(root, (255, 255, 255), nav_rect)
+
+# Cargar y mostrar imágenes
+
+logoImg = pygame.image.load("assets/images/logopaint.png")
+logoImg = pygame.transform.scale(logoImg, (200, 200))
+root.blit(logoImg, (970, -30))
+
+uploadImg = pygame.image.load("assets/images/Upload.png")
+saveImg = pygame.image.load("assets/images/Save.png")
+
+# Crear objetos botón
+
+uploadBtn = Button(30, 50, uploadImg, root)
+uploadBtn.draw()
+saveBtn = Button(100, 50, saveImg, root)
+saveBtn.draw()
+
+# Crear la matriz dentro de la UI
+
+Eng.createMat(root)
+
+# Bucle principal del programa
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    if uploadBtn.pressed():
+        Eng.uploadImg()
+    if saveBtn.pressed():
+        print("Save presionado")
+
+    Eng.drawOverMat()
+
+    pygame.display.update()
+
+pygame.quit()
