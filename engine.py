2abdb59f98ac1f3a3302bc12e728bbb3ac2779a0
Guillermo <guillermosdlg@gmail.com>
2024-05-18 22:50:24 -0600

    Avance 18 de mayo

--- engine.py
+++ engine.py
@@ -8,18 +8,39 @@ class Engine:
         self.rows = 30
         self.cols = 30
         self.cSize = 20
+        self.editorMode = "Brush"
+        self.color = 0
         self.matrix = [[0 for _ in range(self.cols)] for _ in range(self.rows)]
 
     def createMat(self, root):
+        matbbox = pygame.Rect(300, 175, 600, 600)
+        mousepos = pygame.mouse.get_pos()
         for i in range(len(self.matrix)):
             for j in range(len(self.matrix[i])):
                 rect = pygame.Rect(300 + j * self.cSize, 175 + i * self.cSize, self.cSize, self.cSize)
                 pygame.draw.rect(root, (255, 255, 255), rect)
-                pygame.draw.rect(root, (55, 89, 89), rect, 2)
+                pygame.draw.rect(root, (55, 89, 89), rect, 1)
+
+        if matbbox.collidepoint(mousepos) and self.editorMode == "Brush":
+            pygame.mouse.set_visible(False)
+
+            cursor = pygame.image.load("assets/images/brush.png")
+            cursor = pygame.transform.scale(cursor, (50, 50))
+
+            cursorRect = cursor.get_rect()
+            cursorRect.center = (mousepos[0]+20, mousepos[1]-20)
+
+            root.blit(cursor, cursorRect)
+        else:
+            pygame.mouse.set_visible(True)
+
+    def editorModeSelect(self, mode):
+        self.editorMode = mode
+        print(self.editorMode)
 
 
     def drawOverMat(self):
-        if pygame.mouse.get_pressed()[0]:
+        if pygame.mouse.get_pressed()[0] and self.editorMode == "Brush":
             pass
 
 
@@ -89,3 +110,24 @@ class Button:
             self.clicked = False
 
         return command
+
+    def hover(self):
+        if self.rect.collidepoint(pygame.mouse.get_pos()):
+            return True
+        else:
+            return False
+
+
+class SelectedBtn:
+    def __init__(self, x, y, root):
+        self.x = x
+        self.y = y
+        self.root = root
+        self.rect = pygame.Rect(x, y, 55, 55)
+
+    def drawRect(self):
+        rectangle = pygame.draw.rect(self.root, (204, 204, 255), self.rect)
+
+    def changePos(self, x, y):
+        self.rect.x = x
+        self.rect.y = y
--- main.py
+++ main.py
@@ -1,39 +1,69 @@
 import pygame
-from engine import Engine, Button
+from engine import Engine, Button, SelectedBtn
 
 pygame.init()
 
 Eng = Engine()
+isOnButton = False
 icon = pygame.image.load("assets/images/brush.png")
 root = pygame.display.set_mode((1200, 800))
 pygame.display.set_caption("Paintxel")
 pygame.display.set_icon(icon)
 root.fill((223, 223, 229))
 
-# --> Elementos de la interfaz gráfica
-
-nav_rect = pygame.Rect(0, 0, 1200, 150)
-pygame.draw.rect(root, (255, 255, 255), nav_rect)
-
 # Cargar y mostrar imágenes
 
 logoImg = pygame.image.load("assets/images/logopaint.png")
 logoImg = pygame.transform.scale(logoImg, (200, 200))
-root.blit(logoImg, (970, -30))
 
 uploadImg = pygame.image.load("assets/images/Upload.png")
 saveImg = pygame.image.load("assets/images/Save.png")
+eraserImg = pygame.image.load("assets/images/eraser.png")
+blackColorImg = pygame.image.load("assets/images/black.png")
+blueColorImg = pygame.image.load("assets/images/blue.png")
+redColorImg = pygame.image.load("assets/images/red.png")
+greenColorImg = pygame.image.load("assets/images/green.png")
+yellowColorImg = pygame.image.load("assets/images/yellow.png")
+magentaColorImg = pygame.image.load("assets/images/magenta.png")
+pinkColorImg = pygame.image.load("assets/images/pink.png")
+orangeColorImg = pygame.image.load("assets/images/orange.png")
+LBColorImg = pygame.image.load("assets/images/light_blue.png")
+CircleImg = pygame.image.load("assets/images/circle.png")
+squareImg = pygame.image.load("assets/images/square.png")
+
 
 # Crear objetos botón
 
 uploadBtn = Button(30, 50, uploadImg, root)
-uploadBtn.draw()
 saveBtn = Button(100, 50, saveImg, root)
-saveBtn.draw()
+BrushBtn = Button(70, 230, icon, root)
+EraserBtn = Button(150, 230, eraserImg, root)
+CircleBtn = Button(70, 300, CircleImg, root)
+SquareBtn = Button(150, 300, squareImg, root)
+BlackColorBtn = Button(65, 450, blackColorImg, root)
+BlueColorBtn = Button(150, 450, blueColorImg, root)
+RedColorBtn = Button(65, 520, redColorImg, root)
+GreenColorBtn = Button(150, 520, greenColorImg, root)
+YellowColorBtn = Button(65, 590, yellowColorImg, root)
+MagentaColorBtn = Button(150, 590, magentaColorImg, root)
+PinkColorBtn = Button(65, 660, pinkColorImg, root)
+OrangeColorBtn = Button(150, 660, orangeColorImg, root)
+LBColorBtn = Button(105, 730, LBColorImg, root)
 
-# Crear la matriz dentro de la UI
 
-Eng.createMat(root)
+buttonList = [uploadBtn, saveBtn, BrushBtn, EraserBtn, CircleBtn, SquareBtn, BlackColorBtn, BlueColorBtn, RedColorBtn, GreenColorBtn, YellowColorBtn, MagentaColorBtn, PinkColorBtn, OrangeColorBtn, LBColorBtn]
+
+selected = SelectedBtn(70, 230, root)
+
+# Texto de la interfaz
+
+font = pygame.font.Font("fonts/PixelifySans-VariableFont_wght.ttf", 36)
+
+toolbarTxt = font.render("TOOLBAR", True, (0, 0, 0))
+toolbarTxtRect = toolbarTxt.get_rect(center=(133, 200))
+
+colorsTxt = font.render("COLORS", True, (0, 0, 0))
+colorsTxtRect = colorsTxt.get_rect(center=(133, 400))
 
 # Bucle principal del programa
 
@@ -47,9 +77,58 @@ while running:
         Eng.uploadImg()
     if saveBtn.pressed():
         print("Save presionado")
+    if BrushBtn.pressed():
+        Eng.editorModeSelect("Brush")
+        selected.changePos(70, 230)
+    if EraserBtn.pressed():
+        Eng.editorModeSelect("Eraser")
+        selected.changePos(150, 230)
+    if CircleBtn.pressed():
+        Eng.editorModeSelect("Circle")
+        selected.changePos(70, 300)
 
+    root.fill((223, 223, 229))
+
+    nav_rect = pygame.Rect(0, 0, 1200, 150)
+    pygame.draw.rect(root, (255, 255, 255), nav_rect)
+
+    root.blit(logoImg, (970, -30))
+    root.blit(toolbarTxt, toolbarTxtRect)
+    root.blit(colorsTxt, colorsTxtRect)
+
+    Eng.createMat(root)
     Eng.drawOverMat()
 
-    pygame.display.update()
+    selected.drawRect()
+
+    EraserBtn.draw()
+    BrushBtn.draw()
+    CircleBtn.draw()
+    SquareBtn.draw()
+    saveBtn.draw()
+    uploadBtn.draw()
+    BlackColorBtn.draw()
+    BlueColorBtn.draw()
+    RedColorBtn.draw()
+    GreenColorBtn.draw()
+    YellowColorBtn.draw()
+    MagentaColorBtn.draw()
+    PinkColorBtn.draw()
+    OrangeColorBtn.draw()
+    LBColorBtn.draw()
+
+    for i in range(1):
+        for j in buttonList:
+            if j.hover():
+                isOnButton = True
+                break
+            else:
+                isOnButton = False
+        if isOnButton:
+            pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_HAND)
+        else:
+            pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_ARROW)
+
+    pygame.display.flip()
 
 pygame.quit()
